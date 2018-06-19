e771e39baab8cda59fb58c0a59bbce9fb0e2a4ee
Forest Dussault <forest.dussault@canada.ca>
2018-06-19 11:08:01 -0400

    0.0.4

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="BLASTn_Extract",
-    version="0.0.3",
+    version="0.0.4",
     author="Forest Dussault",
     author_email="forest.dussault@canada.ca",
     description="Internal BFSSI package for querying BLASTn results and filtering contig FASTA files",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/bfssi-forest-dussault/BLASTn_Extract",
     packages=setuptools.find_packages(),
     scripts=['BLASTn_Extract/blastn_extract.py'],
